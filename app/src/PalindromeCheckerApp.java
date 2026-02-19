83325f020e60cbf74452f03ab325cee080f37df6
Amrit Roushan <ar5453@srmist.edu.in>
2026-02-19 10:03:30 +0530

    UC1

---
+++ app/src/PalindromeCheckerApp.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class PalindromeCheckerApp {
+    /**
+     MAIN CLASS: UseCase1PalindromeApp
+     This use case introduces fundamental comparison logic
+     before using advanced data structures.
+
+     @author Pushkar Rathi
+     @version 1.0
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to Palindrome Checker App");
+        System.out.println("Version: 2.0");
+        System.out.println("System initialised successfully.");
+    }
+}
