583a37d3e4f6e28bdf6725f3232184a4c73e8b08
Amrit Roushan <ar5453@srmist.edu.in>
2026-02-19 10:10:58 +0530

    UC2 Hardcoded Checker

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -3,15 +3,46 @@ import java.util.Scanner;
 public class PalindromeCheckerApp {
     /**
      MAIN CLASS: UseCase1PalindromeApp
+
+     Use Case 2: Hardcoded Palindrome Validation
+
+     Description:
+     This class demonstrates basic palindrome validation
+     using a hardcoded string value.
+     At this stage, the application:
+     -Stores a predefined string
+     -Compares characters from both ends
+     -Determines whether the string is a palindrome
+     -Displays the result on the console
+
      This use case introduces fundamental comparison logic
-     before using advanced data structure.
+     before using advanced data structures.
 
      @author Pushkar Rathi
-     @version 1.0
+     @version 2.0
      */
     public static void main(String[] args) {
         System.out.println("Welcome to Palindrome Checker App");
         System.out.println("Version: 2.0");
         System.out.println("System initialised successfully.");
+        System.out.println("--------------------------------------------");
+        System.out.println("Enter string to check if palindrome: ");
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        if(checkPalindrome(s))
+            System.out.println(s+" is a palindrome");
+        else
+            System.out.println(s+" is not a palindrome");
+    }
+    public static boolean checkPalindrome(String s){
+        char[] arr = new char[s.length()];
+        arr = s.toCharArray();
+        for (int i = 0; i < s.length()/2; i++) {
+            if (arr[i] == arr[s.length()-i-1])
+                continue;
+            else
+                return false;
+        }
+        return true;
     }
 }
